a68f9473e3684c6e276eb293d7e8c61b8179684d
Mikaela Purugganan <mikaela.programming@gmail.com>
2025-11-22 14:36:31 +0800

    revert prisma version to make it work

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main() {
-  console.log("Seeding users...");
+async function seed() {
   await prisma.user.createMany({
     data: [
-      { name: "Mikki", email: "mikki@example.com" },
-      { name: "Shaui", email: "shaui@example.com" },
+      { name: "Shaun", email: "shaun@example.com" },
+      { name: "Carl", email: "carl@example.com" },
     ],
   });
 }
 
-main()
-  .then(async () => {
-    console.log("Seeding");
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+seed().then(() => prisma.$disconnect());
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript with Express!");
+app.get("/", async (_, res) => {
+  const user = await prisma.user.findUnique({ where: { id: 2 } });
+  res.json(user);
+});
+app.put("/", async (_, res) => {
+  const updatedUser = await prisma.user.update({
+    where: { id: 2 },
+    data: {
+      name: "Shawi",
+      email: "shawi@example.com",
+    },
+  });
+  res.json(updatedUser);
 });
 
 app.listen(port, () => {
